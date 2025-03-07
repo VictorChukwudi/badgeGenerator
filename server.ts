031012c6309c88f5dd4c51e1eb3279fbe351a43b
Victor Ukoha <victorukay@gmail.com>
2025-03-07 21:26:51 +0100

    updates to badge generator

--- server.ts
+++ server.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { generateBadge, deleteBadge } from "./src/generator";
+import {
+  generateBadge,
+  deleteBadge,
+  generateBadge2,
+  generateBadge3,
+} from "./src/generator";
 import { sendBadge } from "./src/mailer";
 
 const app = express();
@@ -13,13 +18,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("🎫 Badge Generator Server is running.");
 });
 
-app.post("/generate", async (req: Request, res: Response) => {
+app.post("/generate/:type", async (req: Request, res: Response) => {
   try {
     const { id, email, fullname } = req.body;
+    const type = req.params.type;
+
     console.log("📥 Received data:", { id, email, fullname });
 
     // Generate badge
-    const badgePath = await generateBadge(id);
+    const badgePath = await generateBadge(id, fullname, type);
 
     // Send badge via email
     await sendBadge(email, badgePath);
@@ -34,6 +41,18 @@ app.post("/generate", async (req: Request, res: Response) => {
   }
 });
 
+app.post("/generate2", async (req: Request, res: Response) => {
+  try {
+    const { id, email, fullname } = req.body;
+    console.log("📥 Received data:", { id, email, fullname });
+    const badgePath = await generateBadge3(id, fullname);
+    console.log(badgePath);
+    res.status(200).send(`✅ Badge generated and sent: ${id}.png`);
+  } catch (error) {
+    console.error("❌ Error handling generate request:", error);
+    res.status(500).send("Error generating badge");
+  }
+});
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
 });
--- src/generator.ts
+++ src/generator.ts
@@ -4,8 +4,13 @@ import { join } from "path";
 import bwipjs from "bwip-js";
 
 const badgeDirectory = "/tmp/badge"; // Writable directory on Render
+const badgeDirectory2 = join(__dirname, "badge");
 
-async function generateBadge(id: string): Promise<string> {
+async function generateBadge(
+  id: string,
+  fullname: string,
+  type: string
+): Promise<string> {
   const canvas = createCanvas(500, 500);
   const ctx = canvas.getContext("2d");
 
@@ -13,11 +18,20 @@ async function generateBadge(id: string): Promise<string> {
     // Ensure the directory exists
     await mkdir(badgeDirectory, { recursive: true });
 
-    const template = await loadImage(join(__dirname, "badge_template2.png"));
+    const tempName =
+      type == "moga"
+        ? "template/official_moga_temp"
+        : "template/official_patriot_temp";
+    const template = await loadImage(join(__dirname, tempName));
     ctx.drawImage(template, 0, 0, 500, 500);
 
-    ctx.font = "bold 20px Arial";
-    ctx.fillStyle = "black";
+    ctx.font = "bold 24px 'Gabriola'";
+    ctx.fillStyle = "#fe0000";
+
+    //Add Fullname
+    ctx.textAlign = "center";
+    ctx.shadowBlur = 50;
+    ctx.fillText(fullname, canvas.width / 2, 378);
 
     // Generate barcode as PNG buffer
     const barcodeBuffer = await bwipjs.toBuffer({
@@ -26,16 +40,112 @@ async function generateBadge(id: string): Promise<string> {
       scale: 1,
       height: 3,
       includetext: true,
+      barcolor: "#935e1d",
       textxalign: "center",
+      textcolor: "#935e1d",
     });
 
     const barcodeImage = await loadImage(barcodeBuffer);
     const barcodeX = (canvas.width - barcodeImage.width) / 2;
-    const barcodeY = 380;
+    const barcodeY = 382;
     ctx.drawImage(barcodeImage, barcodeX, barcodeY);
 
     const buffer = canvas.toBuffer("image/png");
     const badgePath = join(badgeDirectory, `${id}.png`);
+    await writeFile(badgePath, buffer);
+
+    console.log(`✅ Badge saved: ${badgePath}`);
+    return badgePath; // Return path of generated badge
+  } catch (error) {
+    console.error("❌ Error generating badge:", error);
+    throw error;
+  }
+}
+async function generateBadge2(id: string, fullname: string): Promise<string> {
+  const canvas = createCanvas(500, 500);
+  const ctx = canvas.getContext("2d");
+
+  try {
+    // Ensure the directory exists
+    await mkdir(badgeDirectory2, { recursive: true });
+
+    const template = await loadImage(
+      join(__dirname, "templates/official_patriot_temp.png")
+    );
+    ctx.drawImage(template, 0, 0, 500, 500);
+
+    ctx.font = "bold 24px 'Gabriola'";
+    ctx.fillStyle = "#fe0000";
+
+    ctx.textAlign = "center";
+    ctx.shadowBlur = 50;
+    ctx.fillText(fullname, canvas.width / 2, 378);
+    // Generate barcode as PNG buffer
+    const barcodeBuffer = await bwipjs.toBuffer({
+      bcid: "code128",
+      text: id,
+      scale: 1,
+      height: 3,
+      includetext: true,
+      barcolor: "#935e1d",
+      textxalign: "center",
+      textcolor: "#935e1d",
+    });
+
+    const barcodeImage = await loadImage(barcodeBuffer);
+    const barcodeX = (canvas.width - barcodeImage.width) / 2;
+    const barcodeY = 382;
+    ctx.drawImage(barcodeImage, barcodeX, barcodeY);
+
+    const buffer = canvas.toBuffer("image/png");
+    const badgePath = join(badgeDirectory2, `${id}.png`);
+    await writeFile(badgePath, buffer);
+
+    console.log(`✅ Badge saved: ${badgePath}`);
+    return badgePath; // Return path of generated badge
+  } catch (error) {
+    console.error("❌ Error generating badge:", error);
+    throw error;
+  }
+}
+async function generateBadge3(id: string, fullname: string): Promise<string> {
+  const canvas = createCanvas(500, 500);
+  const ctx = canvas.getContext("2d");
+
+  try {
+    // Ensure the directory exists
+    await mkdir(badgeDirectory2, { recursive: true });
+
+    const template = await loadImage(
+      join(__dirname, "templates/official_moga_temp.png")
+    );
+    ctx.drawImage(template, 0, 0, 500, 500);
+
+    ctx.font = "bold 24px 'Gabriola'";
+    ctx.fillStyle = "#fe0000";
+
+    ctx.textAlign = "center";
+    ctx.shadowBlur = 50;
+    ctx.fillText(fullname, canvas.width / 2, 378);
+    // Generate barcode as PNG buffer
+    const barcodeBuffer = await bwipjs.toBuffer({
+      bcid: "code128",
+      text: id,
+      scale: 1,
+      height: 3,
+      includetext: true,
+      barcolor: "#935e1d",
+      textxalign: "center",
+      textcolor: "#935e1d",
+    });
+
+    const barcodeImage = await loadImage(barcodeBuffer);
+    const barcodeX = (canvas.width - barcodeImage.width) / 2;
+    const barcodeY = 382;
+    ctx.drawImage(barcodeImage, barcodeX, barcodeY);
+
+    const buffer = canvas.toBuffer("image/png");
+    const badgePath = join(badgeDirectory2, `${id}.png`);
     await writeFile(badgePath, buffer);
 
     console.log(`✅ Badge saved: ${badgePath}`);
@@ -55,4 +165,4 @@ async function deleteBadge(badgePath: string): Promise<void> {
   }
 }
 
-export { generateBadge, deleteBadge };
+export { generateBadge, generateBadge2, generateBadge3, deleteBadge };
